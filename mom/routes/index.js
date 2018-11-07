10b3f791d8cac25bef0e9052dfb83e0d18fa58cb
konlan mikpe zion <konlanmikpekoah.km@outlook.com>
2018-11-07 01:19:48 +0000

    Update index.js

--- mom/routes/index.js
+++ mom/routes/index.js
@@ -1,101 +1,66 @@
 "use strict"
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var flash = require('connect-flash');
-var messages = require('express-messages');
 var router = express.Router();
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
-var validator = require('express-validator');
+var crypto = require('crypto');
+
+var MongoClient= mongodb.MongoClient;
+
+var Product = require('../schema/products');
+var User = require('../schema/user');
+var Order = require('../schema/orders');
 
 
-var products = require('../schema/products');
-var orders = require('../schema/orders');
-
-
-router.get('/', (req, res, next)=>{
-  req.session.destroy();
-  products.find({category:'breakfast'},function(err, dat){
-    if (err) throw err;
-
-    res.render('user', {dat:dat}
-     
-    );    
-  });
-
-
-});
-
-router.get('/signup', (req, res, next)=>{
-  var cart = req.session.checkout;
-  res.render('signup',{data:cart});
-});
-
-router.post('/checkout', (req, res)=>{
-  var name = req.body.name;
-  var email = req.body.email;
-  var hostel = req.body.hostel;
-  var room = req.body.room;
-  var phone = req.body.phone;
-  req.checkBody("phone", 'You must enter a real Phone number').isLength(9);
-  
-  var errors = req.validationErrors();
-  if(errors){
-    const msg= errors;
-    console.log(msg);
-    req.flash('error',"You must enter a phone number with length 10.");
-    
-    res.redirect('/signup');
-  }else{
-    var data = req.session.checkout;
-    var forder = {
-      name:name,
-      Hostel:hostel,
-      Room:room,
-      phone:phone,
-      mprod:data.mprod,
-      sprod:data.sprod,
-      mqty:data.mqty,
-      sqty:data.sqty,
-      total:data.total,
-      Date:data.Date,
-      Month:data.Month,
-      Year:data.Year
-    }
-   var sve = new orders(forder).save();
-  
-   req.flash('sucess', "Thanks for your order");
-   res.redirect('/');
-   
+/* GET users listing. */
+router.get('/',  (req, res, next)=> {
+  var cart = req.session.cart;
+  var displayCart = {items:[], total:0}
+  var total=0;
+  for (var item in cart){
+    displayCart.items.push(cart[item]);
+    var qr=cart[item].qty;
+    var pri =cart[item].price
+    total +=(qr * pri);
   }
 
-  
-
-});
-//ths for securing the the routes;
-function secur(req, res, next){
-  if(req.session.user){
-    next();
-  }else{
-    res.redirect('/');
-  }
-}
-
-
-
-router.post('/shop', (req, res, next)=>{
-  req.session.mainp = req.session.mainp || {};
-  var mainp = req.session.mainp; 
-  var prod = req.body.prod;
-  var items="items";
-  products.findOne({prodname:prod},function(err, result){
+ var mainp = req.session.mainp.items;
+ displayCart.total =total;
+ 
+ 
+ 
+  var query = Product.find({category:'others'});
+  query.exec((err, result)=>{
     if(err){
-     throw err;
+      throw err;
     }
-    if(mainp[items]){
-      mainp[items].qty++;
+    res.render("checkout.ejs",
+    {cart:displayCart.items,
+      total:total+mainp.price,
+      datat:result,
+      madp:mainp
+
+    });
+
+  })
+  
+
+});
+
+router.post('/add', function(req, res){
+  req.session.cart = req.session.cart || {};
+  var cart = req.session.cart;
+  var prod = req.body.prod;  
+  Product.findOne({prodname:prod},function(err, result){
+    if(err){
+      console.log(err);
+    }
+    if(cart[prod]){
+      cart[prod].qty++;
     }else{
-      mainp[items]= {
+      cart[prod]= {
         item:result._id,
         name:result.prodname,
         price:result.prodprice,
@@ -104,17 +69,102 @@ router.post('/shop', (req, res, next)=>{
       }
     }        
     
-    req.flash('success','You Selected a product please slect your add ons');
+    req.flash('success','You added a product to your cart')
     res.redirect('/users');
     });
   
+ 
 });
 
 
-router.get('/logout', (req, res, next)=>{
-  delete req.session;
-  
-  res.redirect('/');
-})
 
+router.post('/adduser', (req, res, next)=>{
+  var name=req.body.name;
+  var username = req.body.uname;
+  var phone = req.body.uphone;
+  var location = req.body.location;
+  var passd = req.body.password;
+  var passenc = crypto.createHash("md5").update(passd).digest('hex');
+  User.findOne({username:username}, (err, db)=>{
+    if(err){
+      res.redirect("/signup");
+    }
+    if(db==null){
+      var ser = {
+        name:name,
+        phone:phone,
+        username:username,
+        location:location,
+        password:passenc
+      }
+      var las = new User(ser).save();
+      req.flash('sucess', "You sucessfully created an account with US log in now");
+      res.redirect('/');
+   }else{
+    req.flash('error', "User Already Exits");
+    res.redirect('/signup');
+  }
+    
+  });
+  
+  
+});
+
+function secur(req, res, next){
+  if(req.session.user){
+    next();
+  }else{
+    res.redirect('/');
+  }
+}
+
+router.post('/login', (req, res, next)=>{
+  var username = req.body.uname;
+  var passd = req.body.password;
+  var passenc = crypto.createHash("md5").update(passd).digest('hex');
+  var qdr = {
+    username:username,
+    password:passenc
+  }
+  User.findOne(qdr, (err, db)=>{
+    if(err){
+      res.redirect("/signup");
+    }
+    if(db==null){
+      req.flash('error', "Please sign up if you don't have an account");
+      res.redirect("/signup")
+    }else{
+      req.session.user = db;
+      res.redirect('/user')
+    }
+    
+  });
+});
+router.post('/palaceorder', (req, res)=>{
+  var mprod = req.body.mprod;
+  var sprod = req.body.sprod;
+  var tot = req.body.tot;
+  var sqty = req.body.sqty;
+  var mqty = req.body.mqty;
+  var date = new Date();
+  var dete = date.getDate();
+  var month = date.getMonth()+1;
+  var year = date.getFullYear();
+
+
+  var oprod ={
+    sprod:sprod,
+    sqty:sqty,
+    mprod:mprod,
+    mqty:mqty,
+    total:tot,
+    Date:dete,
+    Month:month,
+    Year:year
+  }
+
+  req.session.checkout = oprod;
+  res.redirect('/signup');
+
+});
 module.exports = router;
