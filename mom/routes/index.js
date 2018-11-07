4f5f9d88c5ab48147f5965bc6301d32649c239b0
konlan mikpe zion <konlanmikpekoah.km@outlook.com>
2018-11-07 01:12:51 +0000

    Update index.js

--- mom/routes/index.js
+++ mom/routes/index.js
@@ -1,6 +1,5 @@
 "use strict"
 var express = require('express');
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var flash = require('connect-flash');
 var messages = require('express-messages');
@@ -9,13 +8,70 @@ var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 var validator = require('express-validator');
 
+
 var products = require('../schema/products');
 var orders = require('../schema/orders');
 
-router.get('/signup', (req, res, next)=>{
-  res.render('signup');
+
+router.get('/', (req, res, next)=>{
+  req.session.destroy();
+  products.find({category:'breakfast'},function(err, dat){
+    if (err) throw err;
+
+    res.render('user', {dat:dat}
+     
+    );    
+  });
+
+
 });
 
+router.get('/signup', (req, res, next)=>{
+  var cart = req.session.checkout;
+  res.render('signup',{data:cart});
+});
+
+router.post('/checkout', (req, res)=>{
+  var name = req.body.name;
+  var email = req.body.email;
+  var hostel = req.body.hostel;
+  var room = req.body.room;
+  var phone = req.body.phone;
+  req.checkBody("phone", 'You must enter a real Phone number').isLength(9);
+  
+  var errors = req.validationErrors();
+  if(errors){
+    const msg= errors;
+    console.log(msg);
+    req.flash('error',"You must enter a phone number with length 10.");
+    
+    res.redirect('/signup');
+  }else{
+    var data = req.session.checkout;
+    var forder = {
+      name:name,
+      Hostel:hostel,
+      Room:room,
+      phone:phone,
+      mprod:data.mprod,
+      sprod:data.sprod,
+      mqty:data.mqty,
+      sqty:data.sqty,
+      total:data.total,
+      Date:data.Date,
+      Month:data.Month,
+      Year:data.Year
+    }
+   var sve = new orders(forder).save();
+  
+   req.flash('sucess', "Thanks for your order");
+   res.redirect('/');
+   
+  }
+
+  
+
+});
 //ths for securing the the routes;
 function secur(req, res, next){
   if(req.session.user){
@@ -25,36 +81,7 @@ function secur(req, res, next){
   }
 }
 
-router.get('/user', secur, (req, res, next)=>{
-  var user = req.session.user;
-  var username = user.name;
-  if(req.session.mainp){
-   delete req.session.mainp;
-  }
-  if(req.session.cart){
-    delete req.session.cart;
-   }
-   
-  orders.find({name:username}, (err, result)=>{
-    if (err) throw err;
 
-    products.find({category:'breakfast'},function(err, dat){
-      if (err) throw err;
-
-      res.render('user', {data:result,
-      dat:dat}
-       
-      );    
-    });
-
-    
-  });
-
-});
-
-router.get('/', (req, res, next)=>{
-  res.render('login');
-});
 
 router.post('/shop', (req, res, next)=>{
   req.session.mainp = req.session.mainp || {};
@@ -85,7 +112,8 @@ router.post('/shop', (req, res, next)=>{
 
 
 router.get('/logout', (req, res, next)=>{
-  delete req.session.user;
+  delete req.session;
+  
   res.redirect('/');
 })
 
